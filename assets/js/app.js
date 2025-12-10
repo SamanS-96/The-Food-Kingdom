70ae6478b222a7906ec4bf4c3be3565f015a1f0d
SamanS-96 <samanpsumanasiri@gmail.com>
2025-12-10 11:42:11 +0530

    search meal function

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@ async function loadRandomMeal() {
     await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
     .then(res => res.json())
     .then(data => {
+        //console.log(data);
 
         // elements that should animate
         const elements = [mealName, mealCategory, mealArea, mealTags, mealIns, meal_img];
@@ -41,7 +42,87 @@ async function loadRandomMeal() {
 // initial load
 loadRandomMeal();
 
-// auto change every 10s
+// auto change every 20s
 setInterval(loadRandomMeal, 20000);
+
+
+
+//--Menu Page Functions----------------------------------
+
+let input=document.getElementById("search_meal");
+
+input.addEventListener("keypress",e=>{
+    if(e.key==="Enter"){
+        searchMeal(input.value);
+        input.value="";
+    }
+});
+
+async function searchMeal(mealInput) {
+    let res=await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`);
+    let data=await res.json();
+    // console.log(data);
+    if (!data.meals) {
+        alert("Meal not found!");
+        return;
+    }
+    setMealDetails(data);
+}
+
+function setMealDetails(data){
+    let name=document.getElementById("name");
+    let category=document.getElementById("category");
+    let area=document.getElementById("area");
+    let tags=document.getElementById("tags");
+    let lbl_ingredients=document.getElementById("lbl_ingredients");
+    let lbl_instructions=document.getElementById("lbl_instructions");
+
+    // elements that should animate
+    const elements = [name, category, area, tags, img, meal_video, lbl_ingredients, lbl_instructions];
+
+    // Remove old animation & retrigger
+    elements.forEach(el => {
+        el.classList.remove("animate");
+        void el.offsetWidth; // restart animation trick
+    });
+
+    //********update data------------***************************************
+    name.innerText = data.meals[0].strMeal;
+    category.innerText = data.meals[0].strCategory;
+    area.innerText = "- " + data.meals[0].strArea;
+    tags.innerText = data.meals[0].strTags;
+
+    img.src = data.meals[0].strMealThumb;
+
+    // ---- YOUTUBE FIX ----
+    const youtubeURL = data.meals[0].strYoutube;
+    const videoID = youtubeURL.split("v=")[1];       // Extract ID
+    const embedURL = `https://www.youtube.com/embed/${videoID}`;
+    meal_video.src = embedURL;
+
+    lbl_instructions.innerText=data.meals[0].strInstructions;
+    
+    let ingredients = "";
+    for (let i = 1; i <= 20; i++) {
+        let ingredient = data.meals[0][`strIngredient${i}`];
+        let measure = data.meals[0][`strMeasure${i}`];
+
+        // Stop when there are no more ingredients
+        if (!ingredient || ingredient.trim() === "") break;
+
+        ingredients += `${ingredient} - (${measure})  ,  `;
+    }
+
+    lbl_ingredients.innerText = ingredients;
+
+    //**********------ *************************************************
+    
+    // Add animation again
+    elements.forEach(el => {
+        el.classList.add("animate");
+    });
+
+}
+
 
 
